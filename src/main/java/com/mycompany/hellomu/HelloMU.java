a6430a670688bbd32e9bf2a723ee57377f8bb829
Probook <Probook@DESKTOP-BKFNJ69>
2024-02-03 08:37:21 +0700

    push pertama

---
+++ src/main/java/com/mycompany/hellomu/HelloMU.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hellomu;
+
+/**
+ *
+ * @author Probook
+ */
+public class HelloMU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello MU!, Cageur Bageur Pinter!");
+    }
+}
